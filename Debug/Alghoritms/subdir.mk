f64f5431076d8cbb1e57ef1bdc4a5f41ab1abc0f
Andrianooo <vladosandrianov@yandex.ru>
2026-09-10 13:53:13 +0300

    Внесены изменения для 2-й партии датчиков
    Изменена частота внешнего кварца с 16 на 8 МГц

--- Debug/Alghoritms/subdir.mk
+++ Debug/Alghoritms/subdir.mk
@@ -16,7 +16,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Alghoritms/%.o Alghoritms/%.su Alghoritms/%.cyclo: ../Alghoritms/%.c Alghoritms/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I"C:/Users/Admin/Documents/GitHub/pulse_oximeter/Drivers/W25Q" -I"C:/Users/Admin/Documents/GitHub/pulse_oximeter/Drivers/MAX30102" -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/Admin/Documents/GitHub/pulse_oximeter/Alghoritms" -I../Drivers/W25Q -I"C:/Users/Admin/Documents/GitHub/pulse_oximeter/Libraries/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I"C:/Users/vlado/STM32CubeIDE/workspace_1.12.0/pulse_oximeter/Drivers/W25Q" -I"C:/Users/vlado/STM32CubeIDE/workspace_1.12.0/pulse_oximeter/Drivers/MAX30102" -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/vlado/STM32CubeIDE/workspace_1.12.0/pulse_oximeter/Alghoritms" -I../Drivers/W25Q -I"C:/Users/vlado/STM32CubeIDE/workspace_1.12.0/pulse_oximeter/Libraries/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Alghoritms
 
